77bd737f32b55e8cf8745357f54a789264e2873e
Aherreras98 <112892068+Aherreras98@users.noreply.github.com>
2022-10-05 12:37:25 +0200

    Add files via upload

--- Ejercicio 6/Main.py
+++ Ejercicio 6/Main.py
@@ -1,21 +1,30 @@
+# Crear la clase Vehiculo con los siguientes atributos: Color, Ruedas y Puertas
+# Crear la clase Coche que herede de Vehiculo y tenga los atributos Velocidad y Cilindrada
+# Crear un objeto de la clase coche y mostrarlo por consola
 
-class Vehiculo:
-    color = ""
-    Ruedas = None
-    Puertas = None
+class Vehiculo():
+    # inicializamos los atributos
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
 
-    def cambiarNumRuedas(self, ruedas):
-        self.Ruedas = ruedas
-    def cambiarNumPuertas(self, puertas):
-        self.Puertas = puertas
+    def __str__(self):
+        return "Color {}, {} ruedas".format( self.color, self.ruedas, self.puertas )
 
 class Coche(Vehiculo):
-    Velocidad = 210
-    Cilindrada = 3100
 
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
-c = Coche()
-c.cambiarNumRuedas(4)
-c.cambiarNumPuertas(5)
-print("El coche tiene", c.Ruedas, "ruedas")
-print("El coche tiene", c.Puertas, "puertas")
+    def __str__(self):
+        return "color {}, {} km/h, {} ruedas, {} puertas, {} cc".format( self.color, self.velocidad, self.ruedas, self.puertas, self.cilindrada )
+
+# bloque principal
+# creamos el nuevo objeto, lo inicializamos y se imprime
+coche = Coche("negro", 4, 5, 210, 3100)
+print(coche)
